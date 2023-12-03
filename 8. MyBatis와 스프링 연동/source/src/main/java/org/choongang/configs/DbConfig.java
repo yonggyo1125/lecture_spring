e48345e32b309c0a63b70b76a10b43ac014c88ab
yonggyo1125 <yonggyo00@kakao.com>
2023-12-03 12:34:24 +0900

    강의 소스

---
+++ 8. MyBatis와 스프링 연동/source/src/main/java/org/choongang/configs/DbConfig.java
@@ -0,0 +1,27 @@
+package org.choongang.configs;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DbConfig {
+    @Bean
+    public DataSource dataSource() {
+       DataSource ds = new DataSource();
+
+       ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+       ds.setUrl("jdbc:mysql://localhost:3306/teamb2");
+       ds.setUsername("teamb2");
+       ds.setPassword("_aA123456");
+
+       ds.setMaxActive(10);
+       ds.setInitialSize(2);
+       ds.setTestWhileIdle(true);
+       ds.setMinEvictableIdleTimeMillis(60 * 1000);
+       ds.setTimeBetweenEvictionRunsMillis(5 * 1000);
+
+       return ds;
+    }
+}
